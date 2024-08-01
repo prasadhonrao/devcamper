d7691303db1c4507479dc313c4fbf54b5238b4bc
prasadhonrao <honrao.prasad@gmail.com>
2024-08-01 11:07:49 +0100

    Define bootcamp routes

--- api/server.js
+++ api/server.js
@@ -10,6 +10,26 @@ app.get('/', (req, res) => {
   res.send('Welcome to MERN bootcamp API!');
 });
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Show bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(201).json({ success: true, message: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(204).json({ success: true, message: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
