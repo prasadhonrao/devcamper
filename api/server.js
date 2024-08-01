00c53e55213614fdf77149bcfd8018cc28d19ab7
prasadhonrao <honrao.prasad@gmail.com>
2024-08-01 11:53:46 +0100

    Use morgan package as a logging middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import bootcamps from './routes/bootcamps.js';
+
+const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 5000;
 
 // Load environment variables from .env file
 dotenv.config({ path: './config/config.env' });
@@ -11,10 +15,13 @@ app.get('/', (req, res) => {
   res.send('Welcome to MERN bootcamp API!');
 });
 
+// Define logging middleware using morgan
+if (ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // Define routes
 app.use('/api/v1/bootcamps', bootcamps);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`API server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
