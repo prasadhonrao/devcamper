77dc2ae3ff72d515391834a0679c39162035fffb
prasadhonrao <honrao.prasad@gmail.com>
2024-08-02 08:12:00 +0100

    Implement get all bootcamps and get single bootcamp functions

--- api/controllers/bootcamps.js
+++ api/controllers/bootcamps.js
@@ -3,15 +3,30 @@ import Bootcamp from '../models/Bootcamp.js';
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-const getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+const getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    return res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+  } catch (error) {
+    console.error(error.message.red.bold);
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-const getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, message: `Show bootcamp ${req.params.id}` });
+const getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false, message: `Bootcamp not found with id of ${req.params.id}` });
+    }
+    return res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    console.error(error.message.red.bold);
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create new bootcamp
@@ -22,7 +37,7 @@ const createBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({ success: true, data: bootcamp });
   } catch (error) {
-    console.log(error);
+    console.error(error.message.red.bold);
     res.status(400).json({ success: false });
   }
 };
