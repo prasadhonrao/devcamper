b4bebb3fa038cc1a13053cead25455eb6f2b7118
prasadhonrao <honrao.prasad@gmail.com>
2024-08-02 11:44:05 +0100

    Implement asyncHandler middleware

--- api/controllers/bootcamps.js
+++ api/controllers/bootcamps.js
@@ -1,76 +1,57 @@
 import Bootcamp from '../models/Bootcamp.js';
 import ErrorResponse from '../utils/errorResponse.js';
+import asyncHandler from '../middleware/asyncHandler.js';
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-const getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    return res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
-  } catch (error) {
-    next(error);
-  }
-};
+const getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  return res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-const getBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findById(req.params.id);
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
-    return res.status(200).json({ success: true, data: bootcamp });
-  } catch (error) {
-    next(error);
+const getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
-};
+  return res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-const createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({ success: true, data: bootcamp });
-  } catch (error) {
-    next(error);
-  }
-};
+const createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  return res.status(201).json({ success: true, data: bootcamp });
+});
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-const updateBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
-    res.status(200).json({ success: true, data: bootcamp });
-  } catch (error) {
-    next(error);
+const updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
-};
+  return res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-const deleteBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
-    res.status(200).json({ success: true, data: {} });
-  } catch (error) {
-    next(error);
+const deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
-};
+  return res.status(200).json({ success: true, data: {} });
+});
 
 export { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp };
---
+++ api/middleware/asyncHandler.js
@@ -0,0 +1,2 @@
+const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+export default asyncHandler;
