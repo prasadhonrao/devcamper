7ce2b9fa7357cfbd3a9e70fcfdd5bae027c7d610
prasadhonrao <honrao.prasad@gmail.com>
2024-08-04 21:44:42 +0100

    See data for courses. Add json data files

--- api/seeder.js
+++ api/seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 import Bootcamp from './models/Bootcamp.js';
+import Course from './models/Course.js';
 
 // Connect to MongoDB
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mern-bootcamp-db';
@@ -15,11 +16,13 @@ await mongoose.connect(MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${process.cwd()}/data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${process.cwd()}/data/courses.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (error) {
@@ -32,6 +35,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data deleted...'.green.inverse);
     process.exit();
   } catch (error) {
