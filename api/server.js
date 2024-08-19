f7e2151eafdb517dcd8a5d101ad3995b400f4783
prasadhonrao <honrao.prasad@gmail.com>
2024-08-19 12:05:02 +0100

    Use helmet package to secure express app by setting HTTP response headers.

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ import colors from 'colors';
 import fileUpload from 'express-fileupload';
 import cookieParser from 'cookie-parser';
 import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 import bootcamps from './routes/bootcamps.js';
 import courses from './routes/courses.js';
 import auth from './routes/auth.js';
@@ -32,6 +33,7 @@ const app = express();
 app.use(express.json()); // Body parser
 app.use(cookieParser()); // Cookie parser
 app.use(mongoSanitize()); // Sanitize data
+app.use(helmet()); // Set security headers
 app.use(fileUpload()); // File upload
 app.use(express.static(path.join(path.resolve(), 'public'))); // Set static folder
 app.use('/api/v1/bootcamps', bootcamps);
