c66785bda46992bfec91c07f0bc2e87ed964d06b
prasadhonrao <honrao.prasad@gmail.com>
2024-08-20 09:48:52 +0100

    Update file paths in seeder.js to use new data directory structure

--- api/seeder.js
+++ api/seeder.js
@@ -21,10 +21,10 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mern-bootc
 await mongoose.connect(MONGO_URI);
 
 // Read JSON files
-const bootcamps = JSON.parse(fs.readFileSync(`${process.cwd()}/data/bootcamps.json`, 'utf-8'));
-const courses = JSON.parse(fs.readFileSync(`${process.cwd()}/data/courses.json`, 'utf-8'));
-const users = JSON.parse(fs.readFileSync(`${process.cwd()}/data/users.json`, 'utf-8'));
-const reviews = JSON.parse(fs.readFileSync(`${process.cwd()}/data/reviews.json`, 'utf-8'));
+const bootcamps = JSON.parse(fs.readFileSync(`${process.cwd()}/_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${process.cwd()}/_data/courses.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${process.cwd()}/_data/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${process.cwd()}/_data/reviews.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
