97899e207cf2222992bdb65483cb2b9013a0b719
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-07 22:15:58 +0100

    Create pagination component and add it to BootcampsPage

--- ui/src/pages/bootcamps/BootcampsPage.jsx
+++ ui/src/pages/bootcamps/BootcampsPage.jsx
@@ -1,4 +1,5 @@
 import Bootcamp from '../../components/Bootcamp';
+import Pagination from '../../components/Pagination';
 
 const bootcamps = [
   {
@@ -104,35 +105,7 @@ const BootcampsPage = () => {
             ))}
 
             {/* <!-- Pagination --> */}
-            <nav aria-label="Page navigation example">
-              <ul className="pagination">
-                <li className="page-item">
-                  <a className="page-link" href="/">
-                    Previous
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="/">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="/">
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="/">
-                    3
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="/">
-                    Next
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <Pagination />
           </div>
         </div>
       </div>
