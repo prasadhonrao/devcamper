5d32fdced0686f7f1d7c0d722ece932ac37b8b38
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-09 09:18:38 +0100

    Change default port for API to 3000 for consistency

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ import loadEnvironmentConfig from './config/env.js';
 
 // Load environment variables
 loadEnvironmentConfig();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
