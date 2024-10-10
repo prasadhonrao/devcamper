6c3b612943fb50eb6cf17ea60ec40c71f09ad804
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-10 13:59:26 +0100

    Update spellcheck dictionary and remove console.log stmt

--- api/config/env.js
+++ api/config/env.js
@@ -1,9 +1,7 @@
 import dotenv from 'dotenv';
 
 const loadEnvironmentConfig = () => {
-  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
   dotenv.config();
-  console.log(`Running in ${process.env.NODE_ENV} mode`);
 };
 
 export default loadEnvironmentConfig;
