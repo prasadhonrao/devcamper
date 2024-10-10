a13d4dbd4cc5bcf1fb3dbe37799553e24ba950bd
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-10 16:24:53 +0100

    Refactor db.js to improve MongoDB connection configuration and logging

--- api/config/db.js
+++ api/config/db.js
@@ -3,13 +3,22 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const {
-      mongodb_host = 'localhost', // Default MongoDB host
-      mongodb_port = '27017', // Default MongoDB port
+      mongodb_host = '',
+      mongodb_port = '',
       mongodb_username = '',
       mongodb_password = '',
-      mongodb_db_name = 'devcamper-db', // Default MongoDB database name
+      mongodb_db_name = '',
       mongodb_db_param = '',
     } = process.env;
+
+    // Log environment variables for debugging
+    console.log('Environment variables:');
+    console.log(`mongodb_host: ${mongodb_host}`);
+    console.log(`mongodb_port: ${mongodb_port}`);
+    console.log(`mongodb_username: ${mongodb_username}`);
+    console.log(`mongodb_password: ${mongodb_password}`);
+    console.log(`mongodb_db_name: ${mongodb_db_name}`);
+    console.log(`mongodb_db_param: ${mongodb_db_param}`);
 
     if (!mongodb_host || !mongodb_db_name) {
       throw new Error('mongodb_host and mongodb_db_name must be defined');
@@ -23,9 +32,8 @@ const connectDB = async () => {
 
     mongodb_uri += `${mongodb_host}:${mongodb_port}/${mongodb_db_name}?${mongodb_db_param}`;
 
+    console.log(`MongoDB URI: ${mongodb_uri}`.yellow.bold);
     await mongoose.connect(mongodb_uri);
-
-    console.log(`Connected to MongoDB: ${mongodb_uri}`.yellow.bold);
   } catch (error) {
     console.error(`Error occurred while connecting to MongoDB: ${error.message}`);
     process.exit(1);
