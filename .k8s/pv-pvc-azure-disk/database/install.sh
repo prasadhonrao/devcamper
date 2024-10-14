d8850809ec38c34de96d29468704d6cdded8c5d5
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-14 15:54:50 +0100

    Update PV - PVC with Azure disk YAMLs

--- .k8s/pv-pvc-azure-disk/database/install.sh
+++ .k8s/pv-pvc-azure-disk/database/install.sh
@@ -6,3 +6,4 @@ kubectl apply -f pvc.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service-nodeport.yaml
 kubectl apply -f service-clusterip.yaml
+kubectl apply -f service-loadbalancer.yaml
--- .k8s/pv-pvc-azure-disk/database/uninstall.sh
+++ .k8s/pv-pvc-azure-disk/database/uninstall.sh
@@ -3,6 +3,7 @@ kubectl delete -f configmap.yaml
 kubectl delete -f secret.yaml
 kubectl delete -f service-nodeport.yaml
 kubectl delete -f service-clusterip.yaml
+kubectl delete -f service-loadbalancer.yaml
 kubectl delete -f deployment.yaml
 kubectl delete -f pvc.yaml
 kubectl delete -f pv.yaml
