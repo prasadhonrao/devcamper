acdca7f52f7864c62838981760e49abfac556a81
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-14 09:38:19 +0100

    Refactor code to use NumberFormat helper function and update Courses component
    - Create a new file NumberFormat.js in the helpers directory to handle number formatting using Intl.NumberFormat
    - Update the Courses component to remove the Table import and replace it with a new implementation using the temporary data array
    - Remove the unused Courses.jsx file
    
    Closes #76

--- ui/src/pages/bootcamps/BootcampDetailsPage.jsx
+++ ui/src/pages/bootcamps/BootcampDetailsPage.jsx
@@ -1,37 +1,34 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { FaCheck } from "react-icons/fa";
-import { FaComments } from "react-icons/fa";
-import { FaPencil } from "react-icons/fa6";
-import { FaGlobe } from "react-icons/fa";
 import { Button } from 'react-bootstrap';
-import { FaTimes } from "react-icons/fa";
-
+import { FaCheck, FaComments, FaGlobe, FaTimes } from 'react-icons/fa';
+import { FaPencil } from 'react-icons/fa6';
+import NumberFormat from '../../helpers/NumberFormat';
 
 const BootcampDetailsPage = () => {
-
   const CoursesDetails = [
     {
       id: 1,
       courses_name: 'Front End Web Development',
       duration: 8,
-      description: 'This course will provide you with all of the essentials to become a successful frontend web developer. You will learn to master HTML, CSS and front end JavaScript, along with tools like Git, VSCode and front end frameworks like Vue',
+      description:
+        'This course will provide you with all of the essentials to become a successful frontend web developer. You will learn to master HTML, CSS and front end JavaScript, along with tools like Git, VSCode and front end frameworks like Vue',
       cost: 8000,
       skill: 'Beginner',
-      Scholarship_Available: ''
+      Scholarship_Available: '',
     },
     {
       id: 2,
       courses_name: 'Full Stack Web Development',
       duration: 12,
-      description: 'In this course you will learn full stack web development, first learning all about the frontend with HTML/CSS/JS/Vue and then the backend with Node.js/Express/MongoDB',
+      description:
+        'In this course you will learn full stack web development, first learning all about the frontend with HTML/CSS/JS/Vue and then the backend with Node.js/Express/MongoDB',
       cost: 10000,
       skill: 'Intermediate',
-      Scholarship_Available: ''
+      Scholarship_Available: '',
     },
     // Add more bootcamp data as needed
   ];
-
 
   function redirect_review(e) {
     window.location.href = `/bootcamps/:bootcampId/reviews`;
@@ -39,15 +36,6 @@ const BootcampDetailsPage = () => {
 
   function redirect_addreview(e) {
     window.location.href = `/bootcamps/:bootcampId/reviews/add`;
-  }
-
-  function NumberFormat(amount) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    }).format(amount);
   }
 
   return (
@@ -59,9 +47,14 @@ const BootcampDetailsPage = () => {
             <div className="col-md-8">
               <h1>DevWorks Bootcamp</h1>
               {/* <!-- Description --> */}
-              <p>Devworks is a full stack JavaScript Bootcamp located in the heart of Boston that focuses on the technologies you need to get a high paying job as a web developer</p>
+              <p>
+                Devworks is a full stack JavaScript Bootcamp located in the heart of Boston that focuses on the
+                technologies you need to get a high paying job as a web developer
+              </p>
               {/* <!-- Avg cost --> */}
-              <p className="lead my-4">Average Course Cost: <span className="text-primary">$10,000</span></p>
+              <p className="lead my-4">
+                Average Course Cost: <span className="text-primary">$10,000</span>
+              </p>
               {/* <!-- Courses --> */}
               {CoursesDetails.map((data, key) => (
                 <>
@@ -73,32 +66,49 @@ const BootcampDetailsPage = () => {
                       <ul className="list-group my-3">
                         <li className="list-group-item">Cost: {NumberFormat(data.cost)} USD</li>
                         <li className="list-group-item">Skill Required: {data.skill}</li>
-                        <li className="list-group-item">Scholarship Available: <FaCheck color="green" /> </li>
+                        <li className="list-group-item">
+                          Scholarship Available: <FaCheck color="green" />{' '}
+                        </li>
                       </ul>
                     </div>
                   </Card>
                 </>
               ))}
-
             </div>
             {/* <!-- Sidebar --> */}
             <div className="col-md-4">
               {/* <!-- Image --> */}
               <img src="/images/image_1.jpg" className="img-thumbnail" alt="" />
               {/* <!-- Rating --> */}
-              <h1 className="text-center my-4"><span className="badge badge-secondary badge-success rounded-circle p-3">8.8</span> Rating</h1>
+              <h1 className="text-center my-4">
+                <span className="badge badge-secondary badge-success rounded-circle p-3">8.8</span> Rating
+              </h1>
               {/* <!-- Buttons --> */}
-              <Button onClick={(e) => (redirect_review(e))} className="btn btn-dark btn-block my-3"><FaComments />  Read Reviews</Button>
-              <Button onClick={(e) => (redirect_addreview(e))} className="btn btn-light btn-block my-3"><FaPencil />  Write a Review</Button>
-              <Button onClick={(e) => (redirect_review(e))} className="btn btn-secondary btn-block my-3"><FaGlobe />  Visit Website</Button>
+              <Button onClick={(e) => redirect_review(e)} className="btn btn-dark btn-block my-3">
+                <FaComments /> Read Reviews
+              </Button>
+              <Button onClick={(e) => redirect_addreview(e)} className="btn btn-light btn-block my-3">
+                <FaPencil /> Write a Review
+              </Button>
+              <Button onClick={(e) => redirect_review(e)} className="btn btn-secondary btn-block my-3">
+                <FaGlobe /> Visit Website
+              </Button>
               {/* <!-- Map --> */}
               {/* <div id='map' style={{ width: "100%", height: "300px" }}></div> */}
               {/* <!-- Perks --> */}
               <ListGroup variant="flush">
-                <ListGroup.Item><FaCheck color="green" /> Housing</ListGroup.Item>
-                <ListGroup.Item><FaCheck color="green" /> Job Assistance</ListGroup.Item>
-                <ListGroup.Item><FaTimes color='red' /> Job Guarantee</ListGroup.Item>
-                <ListGroup.Item><FaCheck color="green" /> Accepts GI Bill</ListGroup.Item>
+                <ListGroup.Item>
+                  <FaCheck color="green" /> Housing
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <FaCheck color="green" /> Job Assistance
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <FaTimes color="red" /> Job Guarantee
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <FaCheck color="green" /> Accepts GI Bill
+                </ListGroup.Item>
               </ListGroup>
             </div>
           </div>
