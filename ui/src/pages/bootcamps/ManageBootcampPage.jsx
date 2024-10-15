633469ae89039a62aa5ead56d8d25cb91997f143
shrushtidoshi22 <shrushtidoshi22@gmail.com>
2024-10-15 11:51:59 +0530

    Changes done to ManageBootcampPAge react page

--- ui/src/pages/bootcamps/ManageBootcampPage.jsx
+++ ui/src/pages/bootcamps/ManageBootcampPage.jsx
@@ -17,12 +17,11 @@ const ManageBootcampPage = () => {
                                     <div class="col-md-8">
                                         <div class="card-body">
                                             <h5 class="card-title">
-                                                <a href="bootcamp.html"
+                                                <Link to="/bootcamps/:bootcampId"
                                                     >Devworks Bootcamp
                                                     <span class="float-right badge badge-success"
                                                         >8.8</span
-                                                    ></a
-                                                >
+                                                    ></Link>
                                             </h5>
                                             <span class="badge badge-dark mb-2">Boston, MA</span>
                                             <p class="card-text">
@@ -49,7 +48,7 @@ const ManageBootcampPage = () => {
                             </form>
                             <Link to="/bootcamps/add" className="btn btn-success btn-block">Edit Bootcamp Details</Link>
                             <Link to="/bootcamps/:bootcampId/courses/manage" className="btn btn-primary btn-block">Manage Courses</Link>
-                            <Link to="/bootcamps/:bootcampId/manage" className="btn btn-secondary btn-block">Remove Bootcamp</Link>
+                            <Link to="/bootcamps/:bootcampId/managebootcamp/add" className="btn btn-secondary btn-block">Remove Bootcamp</Link>
 
                             <p class="text-muted mt-5">
                                 * You can only add one bootcamp per account.
