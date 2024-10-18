99f2c3d12fc32ec5fa4dc4347a8bc51945821dab
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-18 10:19:06 +0100

    Add data load animation using react loader spinner

--- ui/src/pages/bootcamps/BootcampsPage.jsx
+++ ui/src/pages/bootcamps/BootcampsPage.jsx
@@ -1,16 +1,26 @@
 import { useState, useEffect } from 'react';
-import { Bootcamp, Pagination } from '../../components';
+import { Oval } from 'react-loader-spinner';
+import { Bootcamp } from '../../components';
 import bootcampService from '../../services/bootcampService';
 
 const BootcampsPage = () => {
   const [bootcamps, setBootcamps] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // Load bootcamps from API
   useEffect(() => {
     const fetchBootcamps = async () => {
-      const fields = ['photo', 'name', 'averageRating', 'location', 'careers', 'id'];
-      const res = await bootcampService.getBootcamps(fields);
-      setBootcamps(res.data);
+      try {
+        const fields = ['photo', 'name', 'averageRating', 'location', 'careers', 'id'];
+        const res = await bootcampService.getBootcamps(fields);
+        setBootcamps(res.data);
+        setFetchError(null);
+      } catch (error) {
+        setFetchError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchBootcamps();
   }, []);
@@ -18,87 +28,15 @@ const BootcampsPage = () => {
   return (
     <section className="browse my-5">
       <div className="container">
-        <div className="row">
-          {/* <!-- Sidebar --> */}
-          <div className="col-md-4">
-            <div className="card card-body mb-4">
-              <h4 className="mb-3">By Location</h4>
-              <form>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control" name="miles" placeholder="Miles From" />
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input type="text" className="form-control" name="zipcode" placeholder="Enter Zipcode" />
-                    </div>
-                  </div>
-                </div>
-                <input type="submit" value="Find Bootcamps" className="btn btn-primary btn-block" />
-              </form>
-            </div>
-
-            {/* <h4>Filter</h4>
-            <form>
-              <div className="form-group">
-                <label> Rating</label>
-                <select className="custom-select mb-2">
-                  <option value="any" selected>
-                    Any
-                  </option>
-                  <option value="9">9+</option>
-                  <option value="8">8+</option>
-                  <option value="7">7+</option>
-                  <option value="6">6+</option>
-                  <option value="5">5+</option>
-                  <option value="4">4+</option>
-                  <option value="3">3+</option>
-                  <option value="2">2+</option>
-                </select>
-              </div>
-
-              <div className="form-group">
-                <label> Budget</label>
-                <select className="custom-select mb-2">
-                  <option value="any" selected>
-                    Any
-                  </option>
-                  <option value="20000">$20,000</option>
-                  <option value="15000">$15,000</option>
-                  <option value="10000">$10,000</option>
-                  <option value="8000">$8,000</option>
-                  <option value="6000">$6,000</option>
-                  <option value="4000">$4,000</option>
-                  <option value="2000">$2,000</option>
-                </select>
-              </div>
-              <input type="submit" value="Find Bootcamps" className="btn btn-primary btn-block" />
-            </form> */}
+        {loading ? (
+          <div className="loader-container">
+            <Oval height="80" width="80" color="red" ariaLabel="oval-loading" />
           </div>
-
-          {/* <!-- Main col --> */}
-          <div className="col-md-8">
-            {bootcamps?.length > 0 ? (
-              bootcamps.map(({ id, photo, name, averageRating, location, careers }) => (
-                <Bootcamp
-                  key={id}
-                  id={id}
-                  photo={photo}
-                  name={name}
-                  averageRating={averageRating}
-                  location={location}
-                  careers={careers}
-                />
-              ))
-            ) : (
-              <h4>No bootcamps found</h4>
-            )}
-            {/* <!-- Pagination --> */}
-            <Pagination />
-          </div>
-        </div>
+        ) : fetchError ? (
+          <p className="text-danger">{fetchError}</p>
+        ) : (
+          bootcamps.map((bootcamp) => <Bootcamp key={bootcamp.id} {...bootcamp} />)
+        )}
       </div>
     </section>
   );
