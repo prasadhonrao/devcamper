e87e590d94eb932d4b153639b74a10d428f8c285
Prasad Honrao <Honrao.Prasad@Gmail.com>
2024-10-22 12:12:07 +0100

    Remove BootcampReviewRating component

--- ui/src/components/index.js
+++ ui/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Bootcamp } from './Bootcamp';
-export { default as BootcampReviewRating } from './BootcampReviewRating';
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Pagination } from './Pagination';
+export { default as Layout } from './Layout';
