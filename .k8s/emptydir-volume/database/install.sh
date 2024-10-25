9e8e12d5a243f6499f30a0315e56b7481320c070
prasadhonrao <honrao.prasad@gmail.com>
2024-10-25 13:20:45 +0100

    Update K8s YAML

--- .k8s/emptydir-volume/database/install.sh
+++ .k8s/emptydir-volume/database/install.sh
@@ -5,3 +5,5 @@ kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
 
+# Port foward deployment to test DB connectivity from local machine
+# kubectl port-forward deployment/devcamper-db-deployment 32017:27017 -n devcamper-namespace
--- .k8s/emptydir-volume/webapi/install.sh
+++ .k8s/emptydir-volume/webapi/install.sh
@@ -4,3 +4,5 @@ kubectl apply -f secret.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
+# Port forward service to test API connectivity from local machine
+# kubectl port-forward service/devcamper-webapi-service 5000:5000 -n devcamper-namespace
