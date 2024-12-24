4f9c57265458ece91575389812ec1e2b8c528a0f
shrushtidoshi22 <shrushtidoshi22@gmail.com>
2024-12-24 14:07:40 +0530

    Some changes made in header and index file for active link highlights

--- src/webapp/src/components/Header.jsx
+++ src/webapp/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Nav, Navbar, Container, Dropdown } from 'react-bootstrap';
 import { FaSignInAlt, FaUserPlus, FaSearch, FaUser, FaSignOutAlt, FaCogs, FaStar } from 'react-icons/fa';
 import { useAuth } from '../contexts/AuthContext';
 
 function Header() {
+  const location = useLocation();
   const { user, logout } = useAuth();
   const navigate = useNavigate();
 
@@ -32,39 +33,39 @@ function Header() {
             <Nav className="ml-auto">
               {user ? (
                 <Dropdown>
-                  <Dropdown.Toggle as={Nav.Link} id="dropdown-basic">
-                    <FaUser /> Account
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item as={Link} to="/bootcamps/manage">
-                      <FaCogs /> Manage Bootcamps
-                    </Dropdown.Item>
-                    <Dropdown.Item as={Link} to="/manage-reviews">
-                      <FaStar /> Manage Reviews
-                    </Dropdown.Item>
-                    <Dropdown.Item as={Link} to="/account/manage">
-                      <FaUser /> Manage Account
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={handleLogout}>
-                      <FaSignOutAlt /> Logout
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              ) : (
-                <>
-                  <Nav.Link as={Link} to="/user/login">
-                    <FaSignInAlt /> Login
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/user/register">
-                    <FaUserPlus /> Register
-                  </Nav.Link>
-                </>
-              )}
-              <div className="nav-separator"></div>
-              <Nav.Link as={Link} to="/bootcamps">
-                <FaSearch /> Browse Bootcamps
-              </Nav.Link>
-            </Nav>
+                <Dropdown.Toggle as={Nav.Link} id="dropdown-basic">
+                  <FaUser /> Account
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item as={Link} to="/bootcamps/:bootcampId/manage" className={location.pathname === '/bootcamps/:bootcampId/manage' ? 'nav-link active' : 'nav-link'}>
+                    <FaCogs /> Manage Bootcamps
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/bootcamps/:bootcampId/reviews/manage" className={location.pathname === '/bootcamps/:bootcampId/reviews/manage' ? 'nav-link active' : 'nav-link'}>
+                    <FaStar /> Manage Reviews
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/user/manage" className={location.pathname === '/user/manage' ? 'nav-link active' : 'nav-link'}>
+                    <FaUser /> Manage Account
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={handleLogout}>
+                    <FaSignOutAlt /> Logout
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/user/login" className={location.pathname === '/user/login' ? 'nav-link active' : 'nav-link'}>
+                  <FaSignInAlt /> Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/user/register" className={location.pathname === '/user/register' ? 'nav-link active' : 'nav-link'}>
+                  <FaUserPlus /> Register
+                </Nav.Link>
+              </>
+            )}
+            <div className="nav-separator"></div>
+            <Nav.Link as={Link} to="/bootcamps" className={location.pathname === '/bootcamps' ? 'nav-link active' : 'nav-link'}>
+              <FaSearch /> Browse Bootcamps
+            </Nav.Link>
+         </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
